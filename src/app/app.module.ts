6bae21980bdbdb18304123589951e127b21e0a4b
John Rykken <john.rykken@gmail.com>
2018-04-02 09:42:44 -0700

    Add appRoutes. Add routing to sign up page, routing to home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AddUserComponent } from './add-user/add-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { ListProjectComponent } from './list-project/list-project.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { routing } from './app.routing';
 
 
 @NgModule({
@@ -19,10 +21,14 @@ import { EditProjectComponent } from './edit-project/edit-project.component';
     EditUserComponent,
     AddProjectComponent,
     ListProjectComponent,
-    EditProjectComponent
+    EditProjectComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const appRoutes: Routes = [ ]
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  }
+]
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
